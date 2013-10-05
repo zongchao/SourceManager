55d07d07fb6bbbcbcce1d257fcbc59bfea9cfca5
zongchao <zongchao314@163.com>
2013-10-05 15:34:28 +0800

    spring mvc demo

--- src/com/neko/controller/TestController.java
+++ src/com/neko/controller/TestController.java
@@ -1,6 +1,17 @@
 package com.neko.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class TestController
 {
-
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String helloWord(Model model) {
+        String message = "Hello World, Spring 3.0!";
+        model.addAttribute("message", message);
+        return "/login/hello";
+    }
 }
